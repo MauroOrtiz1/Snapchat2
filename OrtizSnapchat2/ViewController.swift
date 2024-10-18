fdb755e4a2989a54d1d605b563efbe6bb280febc
MauroOrtiz1 <bernie.ortiz@tecsup.edu.pe>
2024-10-18 15:48:55 -0700

    Initial Commit

---
+++ OrtizSnapchat2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrtizSnapchat2
+//
+//  Created by mauro on 10/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
